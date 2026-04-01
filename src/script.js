fb23ab04599b99a481c2a8da3470ab5d8137d49a
iwezeneneh55-ux <iwezeneneh55@gmail.com>
2026-04-01 15:00:47 +0000

    adding all changes

---
+++ src/script.js
@@ -0,0 +1,15 @@
+let number = 9; //assigned and initialize the variable
+if (number % 2 == 0) //comparing two values
+     {
+        alert(`$number is even`) //what will run if the condition is met
+ } else{
+    alert(`$number is odd`)
+ }
+// alert(+number "is even")
+console.log("the number is even")//what will run if the condition is met
+
+if(){
+
+}else{
+
+}
